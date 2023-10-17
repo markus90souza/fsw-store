842117b35b4a28de86d6a8047e993c4a98087432
rockske8er <rockske8er@gmail.com>
2023-10-17 02:04:43 -0300

    feat: add sheet component

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -2,14 +2,24 @@ import React from 'react'
 import { Card } from '../ui/card'
 import { Button } from '../ui/button'
 import { MenuIcon, ShoppingCartIcon } from 'lucide-react'
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from '../ui/sheet'
 
 export const Header = () => {
   return (
-    <Card className="flex justify-between p-6 items-center">
-      <Button size="icon" variant={'outline'}>
-        <MenuIcon />
-      </Button>
+    <Card className="flex items-center justify-between p-6">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant={'outline'}>
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
 
+        <SheetContent side={'left'}>
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
       <h1 className="text-lg font-semibold">
         <span className="text-primary">FSW</span> Store
       </h1>
